f5a1a0857965e108a956cdbfe7652fb9dfafbcac
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-07-15 09:27:52 +0900

    next/font를 사용해 Paperlogy 폰트 추가 및 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import localFont from 'next/font/local'
+
 
 import '@/styles/global.scss';
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-});
+const paperlogy = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Paperlogy-Medium.ttf',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: '../../public/fonts/Paperlogy-SemiBold.ttf',
+      weight: '500',
+      style: 'normal'
+    }
+  ]
+})
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -25,7 +31,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+			<body className={`${paperlogy.className}`}>
 				{children}
 			</body>
 		</html>
