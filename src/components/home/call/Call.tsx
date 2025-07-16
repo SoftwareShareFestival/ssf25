33e5f7611eb4b1c210bef0c48879967d9453e46d
dlehdrjsg <168471625+truebrid@users.noreply.github.com>
2025-07-16 12:22:26 +0900

    연락처 02로 변경

--- src/components/home/call/Call.tsx
+++ src/components/home/call/Call.tsx
@@ -15,14 +15,14 @@ export default function Schedule() {
 					<span className={s.title}>참가 신청 문의</span>
 					<HStack gap={8}>
 						<PhoneCall className={s.icon} size={20} />
-						<span className={s.phone}>02-717-4616 (선린인고 홍보부)</span>
+						<span className={s.phone}>02-717-4616 (홍보부)</span>
 					</HStack>
 				</VStack>
 				<VStack gap={12}>
 					<span className={s.title}>체험 내용 문의</span>
 					<HStack gap={8}>
 						<PhoneCall className={s.icon} size={20} />
-						<span className={s.phone}>070-4459-9047 (소프트웨어과)</span>
+						<span className={s.phone}>02-713-6219 (소프트웨어과)</span>
 					</HStack>
 				</VStack>
 			</HStack>
