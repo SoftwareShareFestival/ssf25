9588ba80698e9633fd8c62c80facacdb88176bcb
dlehdrjsg <168471625+truebrid@users.noreply.github.com>
2025-07-23 02:28:46 +0900

    헤더 로고 루트 경로 이동추가

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -9,12 +9,14 @@ export default function Header() {
 	return (
 		<Flex fullWidth className={s.full} justify="center">
 			<header className={s.header}>
-				<Image
-					src={'/ssf_white.svg'}
-					height={22}
-					width={55}
-					alt="SSF25 하얀색 로고"
-				/>
+				<Link href="/">
+					<Image
+						src={'/ssf_white.svg'}
+						height={22}
+						width={55}
+						alt="SSF25 하얀색 로고"
+					/>
+				</Link>
 				<button className={s.submitButton}>
 					<Link href="https://lu.ma/92qeg481" style={paperlogy.style} target='_blank'>
 						지원하기
