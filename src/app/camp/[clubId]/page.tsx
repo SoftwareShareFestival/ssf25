c5e3f8f08efbcd07c6c53d0902cecc7f6f281fdf
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-07-24 08:56:04 +0900

    빌드 오류 해결

--- src/app/camp/[clubId]/page.tsx
+++ src/app/camp/[clubId]/page.tsx
@@ -1,15 +1,20 @@
-import { campDataByClub , campData} from '@/data/camp-data';
+import { campDataByClub, campData } from '@/data/camp-data';
 import CampClient from './client';
 
-export default async function CampPage({ params }: { params: { clubId: string } }) {
-    const clubName = decodeURIComponent(params.clubId);
-    const info = campDataByClub[clubName];
-    
-    return <CampClient clubInfo={info} allClubList={campData} />;
+export default async function CampPage({
+	params,
+}: {
+	params: Promise<{ clubId: string }>;
+}) {
+	const { clubId } = await params;
+	const clubName = decodeURIComponent(clubId);
+	const info = campDataByClub[clubName];
+
+	return <CampClient clubInfo={info} allClubList={campData} />;
 }
 
 export async function generateStaticParams() {
-    return Object.keys(campDataByClub).map(clubId => ({
-        clubId: encodeURIComponent(clubId)
-    }));
+	return Object.keys(campDataByClub).map((clubId) => ({
+		clubId: encodeURIComponent(clubId),
+	}));
 }
