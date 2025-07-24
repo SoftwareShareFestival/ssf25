ca8f4f293fc5af5bd46e0a3f5f8e8ac9ce29fa0f
dlehdrjsg <168471625+truebrid@users.noreply.github.com>
2025-07-24 13:33:58 +0900

    카드뉴스 릴스 alert 추가

--- src/components/camp/link/index.tsx
+++ src/components/camp/link/index.tsx
@@ -11,6 +11,11 @@ export default function Link(props: LinkProps) {
 	const { state, link } = props;
 
 	const handleClick = () => {
+        if (state === 'reels' || state === 'cardnews') {
+            alert('각 동아리의 개성 있는 카드뉴스와 릴스는 8월 25일에 공개됩니다!\n자세한 내용은 인스타그램(@sunrin_ssf)에서 확인해 주세요.');
+            return;
+        }
+
 		if (link) {
 			window.open(link, '_blank', 'noopener,noreferrer');
 		}
