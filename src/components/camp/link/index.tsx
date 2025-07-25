d1905c08bc253c6b4a5844cd87ad67e35f3a0f7a
dlehdrjsg <168471625+truebrid@users.noreply.github.com>
2025-07-25 16:51:52 +0900

    ...

--- src/components/camp/link/index.tsx
+++ src/components/camp/link/index.tsx
@@ -21,7 +21,7 @@ export default function Link(props: LinkProps) {
         if (state === 'share') {
             const shareUrl = window.location.href;
             const shareText = title && description
-                ? `2025 소프트웨어나눔축제\n[${title}]\n${description}`
+                ? `2025 소프트웨어 나눔축제\n[${title}]\n${description}`
                 : '선린인터넷고등학교 소프트웨어 나눔 축제 SSF는 2018년부터 이어져온 대표 행사로, 선배들이 소프트웨어 경험을 미래 세대와 나누고, 직접 만들고 체험할 수 있는 기회를 제공하는 축제입니다.';
             if (navigator.share) {
                 navigator.share({
