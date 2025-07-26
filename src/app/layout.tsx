9b5e74452e46ee7bb53ffb30c3ab2f4e5bec0070
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-07-26 22:14:37 +0900

    refactor: restructure metadata to use website object for better maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,32 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import '@/styles/global.scss';
 import { paperlogy } from '@/utils/font';
 
-export const metadata: Metadata = {
+const website = {
 	title: '2025 소프트웨어나눔축제',
 	description: '선린인터넷고등학교 소프트웨어 나눔 축제 SSF는 2018년부터 이어져온 대표 행사로, 선배들이 소프트웨어 경험을 미래 세대와 나누고, 직접 만들고 체험할 수 있는 기회를 제공하는 축제입니다.',
+}
+
+export const metadata: Metadata = {
+	title: website.title,
+	description: website.description,
 	icons: {
 		icon: '/favicon.ico',
 	},
+	openGraph: {
+		title: website.title,
+		description: website.description,
+		images: ['/ssf2025.png'],
+		type: 'website',
+		url: 'https://ssf.sunrin.io',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: website.title,
+		description: website.description,
+		images: ['/ssf2025.png'],
+	},
 };
+
 
 export default function RootLayout({
 	children,
@@ -20,17 +39,6 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<head>
-				<meta property="og:title" content="2025 소프트웨어나눔축제" />
-				<meta property="og:description" content="선린인터넷고등학교 소프트웨어 나눔 축제 SSF는 2018년부터 이어져온 대표 행사로, 선배들이 소프트웨어 경험을 미래 세대와 나누고, 직접 만들고 체험할 수 있는 기회를 제공하는 축제입니다." />
-				<meta property="og:image" content="/ssf2025.png" />
-				<meta property="og:type" content="website" />
-				<meta property="og:url" content="https://ssf25.vercel.app" />
-				<meta name="twitter:card" content="summary_large_image" />
-				<meta name="twitter:title" content="2025 소프트웨어나눔축제" />
-				<meta name="twitter:description" content="선린인터넷고등학교 소프트웨어 나눔 축제 SSF는 2018년부터 이어져온 대표 행사로, 선배들이 소프트웨어 경험을 미래 세대와 나누고, 직접 만들고 체험할 수 있는 기회를 제공하는 축제입니다." />
-				<meta name="twitter:image" content="/ssf2025.png" />
-			</head>
 			<body className={`${paperlogy.className}`}>{children}</body>
 			<Analytics mode="production" />
 			<SpeedInsights />
